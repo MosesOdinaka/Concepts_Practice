e42a0382934ca0c2efe2b0e4bb4cf60a0ecc3ae9
root <root@DESKTOP-SK90JMN>
2024-05-23 15:17:26 +0100

    added html and css concept codes.

--- Foundational_Concepts/Async_program.js
+++ Foundational_Concepts/Async_program.js
@@ -1,9 +1,33 @@
 // Callback
 function fetchData(callback) {
     setTimeout(() => {
-        callback('Datat received');
+        callback('Data received');
     }, 1000);
 }
 
 fetchData(data => console.log(data));
 
+// Promises
+function fetchDataPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Data received');
+        }, 1000);
+    });
+}
+
+fetchDataPromise()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+// Async/Await
+async function fetchDataAsync() {
+    try {
+        const data = await fetchDataPromise();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchDataAsync();
