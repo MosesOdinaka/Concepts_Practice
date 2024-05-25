6fd4992394cca2dc168a7908234c9e7cc1a18e3e
root <root@DESKTOP-SK90JMN>
2024-05-25 15:40:15 +0100

    added new concepts codebase

---
+++ Deployment_and_DevOps/Deployment_Platforms/Vercel_Netlify_for_Frontend.sh
@@ -0,0 +1,13 @@
+# Assuming you have a React project in a GitHub repository
+
+# Steps for vercel
+# 1. Sign in to Vercel and create a new project.
+# 2. Select the GitHub repository to deploy.
+# 3. Configure build settings of necessary (default is usually very fine for Create React AppO).
+# 4. Deploy the site.
+
+# Steps for Netlify
+# 1. Sign in to Netlify and create an new site.
+# 2. Connect to your GitHub repository.
+# 3. Configure build settings if necessafy.
+# 4. Deploy the site
